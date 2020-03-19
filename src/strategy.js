94adf41a71978ddbdf21b5500a91c92a150cb41e
brainbicycle <brian.beckerle@artsymail.com>
2020-03-19 08:34:46 -0400

    Revert "Send back entire id_token"
    This reverts commit f93a3c51f8ae4b5b08a97135b9df2d347ba4f145.

--- src/strategy.js
+++ src/strategy.js
@@ -3,17 +3,18 @@
  * @author: Ananay Arora <i@ananayarora.com>
  */
 
-const OAuth2Strategy = require("passport-oauth2"),
-  crypto = require("crypto"),
-  AppleClientSecret = require("./token"),
-  util = require("util");
-(querystring = require("querystring")), (jwt = require("jsonwebtoken"));
+const OAuth2Strategy = require('passport-oauth2'),
+    crypto = require('crypto'),
+    AppleClientSecret = require("./token"),
+    util = require('util')
+    querystring = require('querystring'),
+    jwt = require('jsonwebtoken');
 
 /**
  * Passport Strategy Constructor
- *
+ * 
  * Example:
- *
+ * 
  *   passport.use(new AppleStrategy({
  *      clientID: "",
  *      teamID: "",
@@ -24,7 +25,7 @@ const OAuth2Strategy = require("passport-oauth2"),
  *       // Here, check if the idToken exists in your database!
  *       cb(null, idToken);
  *   }));
- *
+ *  
  * @param {object} options - Configuration options
  * @param {string} options.clientID – Client ID (also known as the Services ID
  *  in Apple's Developer Portal). Example: com.ananayarora.app
@@ -40,69 +41,60 @@ const OAuth2Strategy = require("passport-oauth2"),
  * @access public
  */
 function Strategy(options, verify) {
-  // Set the URLs
-  options = options || {};
-  options.authorizationURL =
-    options.authorizationURL || "https://appleid.apple.com/auth/authorize";
-  options.tokenURL = options.tokenURL || "https://appleid.apple.com/auth/token";
+    // Set the URLs
+    options = options || {};
+    options.authorizationURL = options.authorizationURL || 'https://appleid.apple.com/auth/authorize';
+    options.tokenURL = options.tokenURL || 'https://appleid.apple.com/auth/token';
 
-  // Make the OAuth call
-  OAuth2Strategy.call(this, options, verify);
-  this.name = "apple";
+    // Make the OAuth call
+    OAuth2Strategy.call(this, options, verify);
+    this.name = 'apple';
 
-  // Initiliaze the client_secret generator
-  const _tokenGenerator = new AppleClientSecret(
-    {
-      client_id: options.clientID,
-      team_id: options.teamID,
-      key_id: options.keyID
-    },
-    options.privateKeyLocation,
-    options.privateKeyString
-  );
+    // Initiliaze the client_secret generator
+    const _tokenGenerator = new AppleClientSecret({
+        "client_id": options.clientID,
+        "team_id": options.teamID,
+        "key_id": options.keyID
+    }, options.privateKeyLocation, options.privateKeyString);
 
-  // Get the OAuth Access Token from Apple's server
-  // using the grant code / refresh token.
+    // Get the OAuth Access Token from Apple's server
+    // using the grant code / refresh token.
 
-  this._oauth2.getOAuthAccessToken = function(code, params, callback) {
-    // Generate the client_secret using the library
-    _tokenGenerator
-      .generate()
-      .then(client_secret => {
-        params = params || {};
-        const codeParam =
-          params.grant_type === "refresh_token" ? "refresh_token" : "code";
-        params[codeParam] = code;
-        params["client_id"] = this._clientId;
-        params["client_secret"] = client_secret;
+    this._oauth2.getOAuthAccessToken = function(code, params, callback) {
+        // Generate the client_secret using the library
+        _tokenGenerator.generate().then((client_secret) => {
+            params = params || {};
+            const codeParam = params.grant_type === 'refresh_token' ? 'refresh_token' : 'code';
+            params[codeParam] = code;
+            params['client_id'] = this._clientId;
+            params['client_secret'] = client_secret;
 
-        const post_data = querystring.stringify(params);
-        const post_headers = {
-          "Content-Type": "application/x-www-form-urlencoded"
-        };
-        this._request(
-          "POST",
-          this._getAccessTokenUrl(),
-          post_headers,
-          post_data,
-          null,
-          function(error, data, response) {
-            if (error) {
-              callback(error);
-            } else {
-              let results = JSON.parse(data);
-              let access_token = results.access_token;
-              let refresh_token = results.refresh_token;
-              let id_token = results.id_token;
-              callback(null, access_token, refresh_token, id_token, results);
-            }
-          }
-        );
-      })
-      .catch(error => {
-        callback(error);
-      });
-  };
+            const post_data = querystring.stringify(params);
+            const post_headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            };
+            this._request(
+                'POST',
+                this._getAccessTokenUrl(),
+                post_headers,
+                post_data,
+                null,
+                function(error, data, response) {
+                    if (error) {
+                        callback(error);
+                    } else {
+                        let results = JSON.parse(data);
+                        let access_token = results.access_token;
+                        let refresh_token = results.refresh_token;
+                        let id_token = jwt.decode(results.id_token).sub;
+                        callback(null, access_token, refresh_token, id_token, results);
+                    }
+                }
+            )
+        }).catch((error) => {
+            callback(error);
+        });
+    }
 }
 
 // Inherit from `OAuth2Strategy`.
@@ -115,7 +107,7 @@ util.inherits(Strategy, OAuth2Strategy);
  * @access protected
  */
 Strategy.prototype.authenticate = function(req, options) {
-  OAuth2Strategy.prototype.authenticate.call(this, req, options);
+    OAuth2Strategy.prototype.authenticate.call(this, req, options);
 };
 
 /**
@@ -124,9 +116,9 @@ Strategy.prototype.authenticate = function(req, options) {
  * @param {object} options
  * @access protected
  */
-Strategy.prototype.authorizationParams = function(options) {
-  options.state = crypto.randomBytes(5).toString("hex");
-  return options;
-};
+Strategy.prototype.authorizationParams = function (options) {
+    options.state = crypto.randomBytes(5).toString('hex');
+    return options;
+}
 
 module.exports = Strategy;
